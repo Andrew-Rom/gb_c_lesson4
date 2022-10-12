211832a0da92a2cb4fe202d548661b4455cbf435
And-Rom <you@example.com>
2022-10-12 21:07:30 +0300

    Task 27 Sum of digits

---
+++ Task27_SumOfDigits/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Задача 27
+Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. */
+
+// Метод считывания
+int GetNumFromUser(string message)
+{
+    Console.Write(message);
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+
+Console.WriteLine("Опеределение суммы цифр в числе.");
+int number = GetNumFromUser("Введите число: ");
+int tempNumber = Math.Abs(number);
+int result = 0;
+while (tempNumber > 0)
+{
+    result = result + tempNumber % 10;
+    tempNumber /= 10;
+}
+Console.WriteLine($"Сумма цифр {number} равна: {result}.");
